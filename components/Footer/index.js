86dee97b01e76bdbbf26c59feeca60abfa5de798
fionaleungg <fionaleung1134@gmail.com>
2026-01-18 23:54:55 -0800

    change resume section, fix cursor lag

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -21,9 +21,14 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link>
+        <a
+          href="https://www.linkedin.com/in/fiona-leung1134/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline underline-offset-1"
+        >
+          <a className="underline underline-offset-1">Fiona Leung</a>
+        </a>
       </h1>
     </>
   );
